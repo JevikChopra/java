a6b7a7dedd662be2da8a953d9e56d4e43fe34ce4
jaivik chopda <jaivikchopda188@gmail.com>
2023-03-16 21:43:08 +0530

    commited

---
+++ R.java
@@ -0,0 +1,14 @@
+class R
+{
+    public static void main(String[] args) 
+
+     
+     {
+        System.out.println("200210116051");
+        String name = "1,2,3,4,5";
+        String[] str = name.split(",");
+        String lastTen = name.substring(name.length()-1);
+        System.out.println(lastTen);
+    
+    }
+}
